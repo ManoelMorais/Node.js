afb01658168d4220885403b5b95b26387defec1d
Manoel <999142818m@gmail.com>
2023-10-31 15:43:51 -0300

    up

--- Projeto/One/src/routers/swagger.route.js
+++ Projeto/One/src/routers/swagger.route.js
@@ -4,12 +4,6 @@ const swaggerRoute = Router();
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json" assert { type: "json" };
 
-if (typeof swaggerDocument === "object") {
-    console.log("JSON importado com sucesso.");
-  } else {
-    console.error("O arquivo JSON não foi importado corretamente.");
-  }
-
 swaggerRoute.use('/', swaggerUi.serve);
 swaggerRoute.get('/', swaggerUi.setup(swaggerDocument))
 
