2997e38a9c9bd74e33e056ff80be8e5265c8f165
Manoel <999142818m@gmail.com>
2023-11-01 16:27:53 -0300

    up fim das config do json da aula 02

--- Aulas/01-Aula/dist/serve.js
+++ Aulas/01-Aula/dist/serve.js
@@ -1,5 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sum_1 = require("./sum");
-console.log((0, sum_1.sum)());
-//# sourceMappingURL=serve.js.map
+"use strict";console.log(123);console.log(12345);
--- Aulas/02-Aula/src/serve.ts
+++ Aulas/02-Aula/src/serve.ts
@@ -1,3 +1,2 @@
 console.log(123);
 console.log(12345);
-
